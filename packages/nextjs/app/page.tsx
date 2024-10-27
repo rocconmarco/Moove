7208a8ed265cc76937147551d2eb5c7a1dd13d5a
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-10-27 12:07:26 +0100

    Added 3d map image with gradient

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -6,15 +6,16 @@ import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 import Link from "next/link";
 import { ImageSlider } from "~~/components/ImageSlider";
+import styles from "./HomePage.module.css"
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
 
   return (
     <div className="relative min-h-screen w-full">
-      <div className="relative z-10 flex items-center flex-col flex-grow pt-6">
+      <div className="relative z-10 flex items-center flex-col flex-grow pt-8">
         <div className="px-5">
-          <h1 className="text-center">
+          <h1 className={`${styles.title} text-center`}>
             <span className="block text-5xl mb-2 text-white shadow-inner">Get your pass to the future of</span>
             <span className="block text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-white via-slate-200 to-gray-600 drop-shadow-[0_10px_8px_rgba(0,0,0,0.8)] pb-2">sustainable mobility</span>
           </h1>
@@ -44,6 +45,11 @@ const Home: NextPage = () => {
               packages/hardhat/contracts
             </code>
           </p> */}
+        </div>
+
+        <div className="flex space-x-96">
+          <p className="absolute left-[5%]">Alla grande</p>
+          <img src="/3d-map/3d-map.jpg" className={`${styles.images}`}></img>
         </div>
 
         {/* <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
