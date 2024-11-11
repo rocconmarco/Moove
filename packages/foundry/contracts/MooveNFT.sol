189edf8798338ca53eeb0fcdfd93afc88f0abee8
Marco Roccon <marcoroccon@MBP-di-Marco-2.homenet.telecomitalia.it>
2024-11-11 12:44:38 +0100

    MooveNFT commented with NatSpecs

--- packages/foundry/contracts/MooveNFT.sol
+++ packages/foundry/contracts/MooveNFT.sol
@@ -6,6 +6,14 @@ import { IMintableNFT } from "./IMintableNFT.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+
+/**
+ * A smart contract that manages the minting process of MooveNFT
+ * @author Marco Roccon
+ * @dev The contract inherits from the ERC721 and Ownable contract by OpenZeppelin
+ * @dev The contract communicates with the AuctionAlpha via a custom interface that only implements
+ * @dev the required functions, which are mint, safeMint and getMaxSupply
+ */
 contract MooveNFT is ERC721, IMintableNFT, Ownable {
 
   error MooveNFT__MintingNotAuthorized();
@@ -19,20 +27,35 @@ contract MooveNFT is ERC721, IMintableNFT, Ownable {
   string private s_baseURI;
   uint256 private s_maxSupply;
 
-  // A mapping of all the addresses authorized to mint NFTs
-  // created to avoid unathorized minting of NFTs directly from this contract
-  // Users must win the auction to mint the NFT
-  // Ideally, only the AuctionAlpha address should be authorized
-  // This mapping is created because of the impossibility to pass the AuctionAlpha address while deploying
-  // since the AuctionAlpha contract also needs MooveNFT's address to be correctly deployed
+  /**
+   * A mapping of all the addresses authorized to mint NFTs
+   * created to avoid unathorized minting of NFTs directly from this contract
+   * Users must win the auction to mint the NFT
+   * @dev Ideally, only the AuctionAlpha address should be authorized
+   * @dev This mapping is created because of the impossibility to pass the AuctionAlpha address while deploying
+   * @dev since the AuctionAlpha contract also needs MooveNFT's address to be correctly deployed
+   */ 
   mapping (address => bool) private s_authorizedMinters;
 
+
+  /**
+   * @param baseURI the base URI for Moove NFTs' metadata stored in IPFS, the format of the string should be ipfs://<CID>
+   * The ERC721 token is inizialized with the name "MooveNFT" and with the symbol "MOOVE"
+   * The owner of the contract is set to be the sender of the deployment transaction
+   */
   constructor(string memory baseURI) ERC721("MooveNFT", "MOOVE") Ownable(msg.sender) {
     s_baseURI = baseURI;
     s_tokenCounter = 0;
     s_maxSupply = 13;
   }
 
+  /**
+   * @param to recipient address
+   * @param tokenId id of the NFT to be minted
+   * @dev Implementation of the mint function from the ERC721 standard
+   * @dev It checks whether the sender is an authorized address
+   * @dev If minted successfully, the token counter will increment
+   */
   function mint(address to, uint256 tokenId) external {
     if(to == address(0)) {
       revert MooveNFT__NotAValidAddress();
@@ -49,6 +72,15 @@ contract MooveNFT is ERC721, IMintableNFT, Ownable {
     }
   }
 
+  /**
+   * @param to recipient address
+   * @param tokenId id of the NFT to be minted
+   * @dev Implementation of the safeMint function from the ERC721 standard
+   * @dev It checks whether the sender is an authorized address
+   * @dev If minted successfully, the token counter will increment
+   * @dev The AuctionAlpha contract utilizes safeMint to mint the NFT to the winner of the auction
+   * @dev We prefer to use safeMint to check the receiver's capacility to accept NFTs
+   */
   function safeMint(address to, uint256 tokenId) external {
     if(to == address(0)) {
       revert MooveNFT__NotAValidAddress();
@@ -65,6 +97,13 @@ contract MooveNFT is ERC721, IMintableNFT, Ownable {
     }
   }
 
+
+  /**
+   * @param minter address to be authorized
+   * @dev This function allows the owner of the contract to grant minting rights over the NFT collection
+   * @dev to a particular address
+   * @dev It updates the mapping of authorized minters
+   */
   function addAuthorizedMinter(address minter) public onlyOwner {
     if(minter == address(0)) {
       revert MooveNFT__NotAValidAddress();
@@ -72,6 +111,12 @@ contract MooveNFT is ERC721, IMintableNFT, Ownable {
     s_authorizedMinters[minter] = true;
   }
 
+    /**
+   * @param minter address to be removed from the list of authorized minters
+   * @dev This function allows the owner of the contract to revoke minting rights
+   * @dev over the NFT collection for a particular address
+   * @dev It updates the mapping of authorized minters
+   */
   function removeAuthorizedMinter(address minter) public onlyOwner {
     s_authorizedMinters[minter] = false;
   }
